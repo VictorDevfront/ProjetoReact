9ca8e251b362f925b6082a11a4d397e986f79916
Victor Nascimento <Vitiin123456@gmail.com>
2023-11-17 20:17:47 -0300

    Ok
    ok

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+
 function App() {
   return (
-    <div className="App">
-    <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+    <Link to="/">Home</Link>
+    <Link to="/contact">Contato</Link>
+    <Link to="/company">Empresa</Link>
+    <Link to="/newproject">Novo projeto</Link>   
+      </div>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route exact path="/company">
+      <Home />
+    </Route>
+    <Route exact path="/contact">
+      <Home />
+    </Route>
+    <Route exact path="/newproject">
+      <NewProject />
+    </Route>
+    <p>Footer</p>
+    </Router>
   )
 }
+
 export default App
